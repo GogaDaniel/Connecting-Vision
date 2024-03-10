c092351ca1c445cd392cfa9fb4735e6ca923faad
bneiG1 <105239758+bneiG1@users.noreply.github.com>
2024-03-10 16:50:12 +0200

    working on routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './content/home/home.component';
+import { AboutComponent } from './content/about/about.component';
+import { ServicesComponent } from './content/services/services.component';
+import { ContactComponent } from './content/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'contact', component: ContactComponent },
+];
