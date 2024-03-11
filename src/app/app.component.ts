92d0f6e7a3b2aab89f627b199d1a81152607b7ee
bneiG1 <105239758+bneiG1@users.noreply.github.com>
2024-03-12 01:25:16 +0200

    backup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
-import { ContentComponent } from "./content/content.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, FooterComponent, ContentComponent]
+    imports: [RouterOutlet, RouterLinkActive, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'connecting-vision';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { HomeComponent } from './content/home/home.component';
 import { AboutComponent } from './content/about/about.component';
 import { ServicesComponent } from './content/services/services.component';
 import { ContactComponent } from './content/contact/contact.component';
+import { PageNotFoundComponent } from './content/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'contact', component: ContactComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
 ];
