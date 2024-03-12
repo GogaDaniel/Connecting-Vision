c529292fedb9045f5650af19058d3716b67bb9b8
bneiG1 <105239758+bneiG1@users.noreply.github.com>
2024-03-12 02:22:52 +0200

    test

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,16 +4,16 @@ import { routes } from './app.routes';
 
 import { provideClientHydration } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import {
-  getAnalytics,
-  provideAnalytics,
-  ScreenTrackingService,
-} from '@angular/fire/analytics';
-import { getPerformance, providePerformance } from '@angular/fire/performance';
-import {
-  getRemoteConfig,
-  provideRemoteConfig,
-} from '@angular/fire/remote-config';
+// import {
+//   getAnalytics,
+//   provideAnalytics,
+//   ScreenTrackingService,
+// } from '@angular/fire/analytics';
+// import { getPerformance, providePerformance } from '@angular/fire/performance';
+// import {
+//   getRemoteConfig,
+//   provideRemoteConfig,
+// } from '@angular/fire/remote-config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -34,9 +34,9 @@ export const appConfig: ApplicationConfig = {
         })
       )
     ),
-    importProvidersFrom(provideAnalytics(() => getAnalytics())),
-    ScreenTrackingService,
-    importProvidersFrom(providePerformance(() => getPerformance())),
-    importProvidersFrom(provideRemoteConfig(() => getRemoteConfig())), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"connecting-vision","appId":"1:1058693252524:web:1d7b49eb650d7ba81a2609","databaseURL":"https://connecting-vision-default-rtdb.europe-west1.firebasedatabase.app","storageBucket":"connecting-vision.appspot.com","apiKey":"AIzaSyCmR9dCRqYGWiQwZAuXtpBFrjoHbdjq96I","authDomain":"connecting-vision.firebaseapp.com","messagingSenderId":"1058693252524","measurementId":"G-QJ7HQWDP6L"}))), importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService, importProvidersFrom(providePerformance(() => getPerformance())), importProvidersFrom(provideRemoteConfig(() => getRemoteConfig())),
+    // importProvidersFrom(provideAnalytics(() => getAnalytics())),
+    // ScreenTrackingService,
+    // importProvidersFrom(providePerformance(() => getPerformance())),
+    // importProvidersFrom(provideRemoteConfig(() => getRemoteConfig())), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"connecting-vision","appId":"1:1058693252524:web:1d7b49eb650d7ba81a2609","databaseURL":"https://connecting-vision-default-rtdb.europe-west1.firebasedatabase.app","storageBucket":"connecting-vision.appspot.com","apiKey":"AIzaSyCmR9dCRqYGWiQwZAuXtpBFrjoHbdjq96I","authDomain":"connecting-vision.firebaseapp.com","messagingSenderId":"1058693252524","measurementId":"G-QJ7HQWDP6L"}))), importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService, importProvidersFrom(providePerformance(() => getPerformance())), importProvidersFrom(provideRemoteConfig(() => getRemoteConfig())),
   ],
 };
