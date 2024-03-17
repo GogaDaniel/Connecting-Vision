32ece261b0aee3d080b59167d448ee98169b9104
bneiG1 <105239758+bneiG1@users.noreply.github.com>
2024-03-18 01:32:59 +0200

    added sitemap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,25 @@ import { HomeComponent } from './content/home/home.component';
 import { AboutComponent } from './content/about/about.component';
 import { ServicesComponent } from './content/services/services.component';
 import { ContactComponent } from './content/contact/contact.component';
-import { PageNotFoundComponent } from './content/page-not-found/page-not-found.component';
+// import { PageNotFoundComponent } from './content/page-not-found/page-not-found.component';
+// import { SitemapComponent } from './sitemap/sitemap.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'contact', component: ContactComponent },
-  { path: '',   component: HomeComponent  }, // redirect to `home`
-  { path: '**', component: HomeComponent },  
+  // {
+  //   path: 'sitemap',
+  //   component: SitemapComponent,
+  //   data: {
+  //     loc: '/',
+  //     lastmod: '2022-04-07',
+  //     changefreq: 'daily',
+  //     priority: '1.0',
+  //   },
+  // },
+  { path: '', component: HomeComponent }, // redirect to `home`
+  { path: '**', component: HomeComponent },
 ];
+
